eed09813b986941b5f666dd8fb8c057429950f90
Bruno Pavese <pavese.bruno@gmail.com>
2023-06-06 16:46:40 -0300

    config: limpando o frontend

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default async function Home() {
   const data = await getData();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-2xl">Contagem: {data}</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-zinc-800">
+      <h1 className="font-bold text-2xl text-slate-100">Contagem: {data}</h1>
     </main>
   );
 }
