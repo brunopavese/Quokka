6f7ae2e47394cc191d06b10c96230b4544edff1b
Bruno Pavese <pavese.bruno@gmail.com>
2023-06-09 00:42:29 -0300

    feature: criada a pagina de cadastro

--- web/src/app/(auth)/register/page.tsx
+++ web/src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import { UnableSendButton } from '@/components/UnableSendButton';
 import { XClose } from '@/components/XClose';
 import Link from 'next/link';
 
-export default async function Login() {
+export default async function Register() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-20 px-8 md:py-8 bg-zinc-800">
       <div className="flex flex-col items-center">
@@ -11,19 +11,27 @@ export default async function Login() {
         <div className="flex flex-col rounded-md bg-zinc-600 px-4 py-6 w-90vw sm:w-50vw lg:w-30rem justify-center mb-2">
           <div className="flex flex-col items-center">
             <h1 className="font-medium text-2xl text-slate-100 mb-6 align-baseline">
-              Sign in with your account
+              Sign up
             </h1>
           </div>
           {/* Username Input */}
           <div className="flex flex-col">
             <h2 className="text-zinc-200 font-semibold text-sm mb-2">
-              USERNAME or EMAIL
+              USERNAME
             </h2>
             <input className="font-normal px-2 py-3 text-base text-slate-100 rounded-md bg-zinc-700 inline-block max-w-full focus:outline-none focus:ring-0" />
             <div className="flex h-6 items-center justify-end">
               <p className="text-xs text-red-400 font-medium">
                 Campo obrigatório
               </p>
+            </div>
+          </div>
+          {/* Email Input */}
+          <div className="flex flex-col">
+            <h2 className="text-zinc-200 font-semibold text-sm mb-2">EMAIL</h2>
+            <input className="font-normal px-2 py-3 text-base text-slate-100 rounded-md bg-zinc-700 inline-block max-w-full focus:outline-none focus:ring-0" />
+            <div className="flex h-6 items-center justify-end">
+              <p className="text-xs text-red-400 font-medium"></p>
             </div>
           </div>
           {/* Password Input */}
@@ -38,15 +46,27 @@ export default async function Login() {
               </p>
             </div>
           </div>
+          {/* Confirm Password Input */}
+          <div className="flex flex-col">
+            <h2 className="text-zinc-200 font-semibold text-sm mb-2">
+              CONFIRM PASSWORD
+            </h2>
+            <input className="font-normal px-2 py-3 text-base text-slate-100 rounded-md bg-zinc-700 inline-block max-w-full focus:outline-none focus:ring-0" />
+            <div className="flex h-6 items-center justify-end">
+              <p className="text-xs text-red-400 font-medium">
+                Campo obrigatório
+              </p>
+            </div>
+          </div>
           <div className="flex flex-row items-center justify-between mt-2">
             <div className="inline-flex flex-row">
               <p className="font-normal text-sm text-slate-100">
-                Don't have any account?{' '}
+                Already have an account?{' '}
                 <Link
-                  href="/register"
+                  href="/login"
                   className="font-normal text-sm text-orange-500 hover:underline focus:underline"
                 >
-                  Register
+                  Login
                 </Link>
               </p>
             </div>
--- web/src/components/XClose.tsx
+++ web/src/components/XClose.tsx
@@ -6,7 +6,7 @@ export function XClose() {
   return (
     <button
       type="submit"
-      className="flex p-2 w-10 h-10 justify-center items-center text-3xl font-semibold text-zinc-400 rounded-full hover:bg-zinc-600 focus:outline-none focus:bg-orange-500"
+      className="flex p-2 w-10 h-10 justify-center items-center text-3xl font-semibold text-zinc-400 rounded-full hover:bg-zinc-600 focus:outline-none focus:bg-orange-500 focus:text-zinc-100"
     >
       <X />
     </button>
