a57ae85dc44a2fb2f99f2a08c3c7e9c8ab5f9534
Bruno Pavese <bruno@ejcm.com.br>
2023-11-21 01:21:41 -0300

    alterado o seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,14 +1,31 @@
+import axios from 'axios';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+async function downloadImage(url: string) {
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  return Buffer.from(response.data, 'binary');
+}
+
 async function main() {
+  const avatar1 = await downloadImage(
+    'https://i.pinimg.com/550x/0e/51/7e/0e517eb57cb5a992ef3230b0e0d792af.jpg'
+  );
+  console.log('Avatar1:', avatar1);
+  const avatar2 = await downloadImage(
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjlzKpsgPA-Icjs5ZZ9g-sdHEA8D1Okjmsxw&usqp=CAU'
+  );
+
+  const avatar3 = await downloadImage(
+    'https://creativefreedomrpg.com/download/file.php?avatar=113360_1523801896.jpg'
+  );
+
   const user = await prisma.user.create({
     data: {
       userName: 'ShadowBlade',
       email: 'ShadowBlade@email.com',
-      avatarUrl:
-        'https://i.pinimg.com/550x/0e/51/7e/0e517eb57cb5a992ef3230b0e0d792af.jpg',
+      avatarImage: avatar1,
     },
   });
 
@@ -16,8 +33,7 @@ async function main() {
     data: {
       userName: 'Stormbringer',
       email: 'Stormbringer@email.com',
-      avatarUrl:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjlzKpsgPA-Icjs5ZZ9g-sdHEA8D1Okjmsxw&usqp=CAU',
+      avatarImage: avatar2,
     },
   });
 
@@ -25,8 +41,7 @@ async function main() {
     data: {
       userName: 'NovaStar',
       email: 'NovaStar@email.com',
-      avatarUrl:
-        'https://creativefreedomrpg.com/download/file.php?avatar=113360_1523801896.jpg',
+      avatarImage: avatar3,
     },
   });
 
